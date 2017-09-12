509ca8deab509de7bcc69721743d4a161c80e870
Johnathan Phan <johnathan.phan@JPhan-mpb3.local>
2017-09-12 16:20:01 +0100

    correct location to all be in this folder

--- alias.sh
+++ alias.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 setToken() {
     #do things with parameters like $1 such as
-    ~/aws-mfa-script/mfa.sh $1 $2
+    . ~/aws-mfa-script/mfa.sh $1 $2
     source ~/aws-mfa-script/.token_file
     echo "Your creds have been set in your env."
 }
