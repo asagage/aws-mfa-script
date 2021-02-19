80997e3a5575d9b92e4c03cd91649d161d58ae48
Adele Scott <adele@null>
2021-02-19 13:52:18 +1100

    Allow files to be extracted to any directory, rather than forcing use of home directory

--- alias.sh
+++ alias.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+DIR="$(dirname $(realpath ${BASH_SOURCE}))"
 setToken() {
-    ~/mfa.sh $1 $2
-    source ~/.token_file
+    $DIR/mfa.sh $1 $2
+    source $DIR/.token_file
     echo "Your creds have been set in your env."
 }
 alias mfa=setToken
