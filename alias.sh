b97b7acb573d2a1115e822d2e4bd3db01871dbe5
Kirsty Munro <kirsty.munro@myproteus.com>
2022-08-22 11:09:55 +0100

    Set condition to look for zsh or bash and set variable

--- alias.sh
+++ alias.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-SCRIPT_PATH="$( cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 ; pwd -P  )"
+
+if [ ! -z ${ZSH_VERSION+x} ]; then
+    echo "zsh set SCRIPT_PATH variable"
+    echo ${(%):-%x}
+    SCRIPT_PATH="$( cd -- "$(dirname -- "${(%):-%x}")" >/dev/null 2>&1 ; pwd -P  )"
+elif [ ! -z ${BASH_VERSION+x} ]; then
+    echo "bash set SCRIPT_PATH variable"
+    echo $BASH_SOURCE
+    SCRIPT_PATH="$( cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 ; pwd -P  )"
+else
+    echo "shell not recognised"
+    exit 1
+fi
+
 setToken() {
     $SCRIPT_PATH/mfa.sh $1 $2
     source ~/.token_file
