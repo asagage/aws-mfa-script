dfed579b2b26de8ff6dc143a2d31431e7961abca
Héctor Pablos <hector@stylesage.co>
2022-08-25 13:19:21 +0200

    Correctly set SCRIPT_PATH when using zsh

--- alias.sh
+++ alias.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-SCRIPT_PATH="$( cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 ; pwd -P  )"
+SHELL_PROCESS=`ps -p $$ | sed -n 2p`
+if [[ "${SHELL_PROCESS}" == *"zsh"* ]]; then
+  RELEVANT_BASH_SOURCE="${(%):-%N}"
+else
+  RELEVANT_BASH_SOURCE="${BASH_SOURCE[0]}"
+fi
+SCRIPT_PATH="$( cd -- "$(dirname -- "${RELEVANT_BASH_SOURCE}")" >/dev/null 2>&1 ; pwd -P  )"
 setToken() {
     $SCRIPT_PATH/mfa.sh $1 $2
     source ~/.token_file
